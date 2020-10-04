3aa25530441badfb4978f05171722a2f676f8947
Андрей Ефимов <efiand@ya.ru>
2020-10-04 15:37:06 +0300

    Прогрессивное улучшение скрытия формы

--- source/js/components/page.js
+++ source/js/components/page.js
@@ -1,7 +1,7 @@
 export default class Page {
 	constructor(container) {
 		this._footer = container.querySelector(`.page__footer`);
-		document.documentElement.style.setProperty(`--height-footer`, `${this._footer.clientHeight}px`);
+		document.documentElement.style.setProperty(`--footer-height-amendment`, `${this._footer.clientHeight}px`);
 
 		if (container.classList.contains(`page--index`)) {
 			container.classList.add(`page--index-ready`);
--- source/js/components/search.js
+++ source/js/components/search.js
@@ -4,6 +4,14 @@ export default class Search {
 			return;
 		}
 
+		// Переключение показа формы
+		this._form = container.querySelector(`.search__form`);
+		this._formToggler = container.querySelector(`.search__button`);
+		this._formToggler.removeAttribute(`disabled`);
+		this._formToggler.addEventListener(`click`, () => {
+			this._form.classList.toggle(`search__form--opened`);
+		});
+
 		// Показ неактуальных без загрузки скрипта элементов форм
 		for (const hiddenElement of container.querySelectorAll(`[hidden]`)) {
 			hiddenElement.removeAttribute(`hidden`);
